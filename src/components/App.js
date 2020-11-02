272e0da5b87176464bb50338471e66f674a1eef0
Emmanuel <fegomson@gmail.com>
2020-11-02 08:28:54 -0600

    create state in the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import React, {Component} from "react"
 import '../css/App.css';
 import '../css/index.css';
 
@@ -6,22 +7,30 @@ import AddAppointments from "./AddAppointments"
 import SearchAppointments from './SearchAppointments'
 import ListAppointments from "./ListAppointments"
 
-const App = () => {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments/>
-              <SearchAppointments/>
-              <ListAppointments/>
+class App extends Component {
+  state = {
+    myLastName: "Egomson"
+  }
+
+  render() {
+    const {myLastName} = this.state
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {myLastName}
+                <AddAppointments/>
+                <SearchAppointments/>
+                <ListAppointments/>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default App;
