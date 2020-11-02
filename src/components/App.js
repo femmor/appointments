c930497ae5b5a285e27648ebf47ec6d3eb34ac11
Emmanuel <fegomson@gmail.com>
2020-11-02 08:52:12 -0600

    using fetch in a lifecycle method

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,37 @@ import ListAppointments from "./ListAppointments"
 
 class App extends Component {
   state = {
-    myLastName: "Egomson"
+    appointments: []
   }
 
+  componentDidMount() {
+    fetch("./data.json")
+    .then(
+      response => response.json() 
+    )
+    .then(
+      result => {
+        const apt = result.map(appointment => {
+          return appointment
+        })
+
+        this.setState({
+          appointments: apt
+        })
+      }
+    )
+  }
+  
+
   render() {
-    const {myLastName} = this.state
+    const {aptDate, aptNotes, ownerName, petName} = this.state.appointments
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                {myLastName}
+                {}
                 <AddAppointments/>
                 <SearchAppointments/>
                 <ListAppointments/>
